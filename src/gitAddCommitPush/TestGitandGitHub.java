38937431f7ba6c7b63777d67f06caa03eac48178
xpraxr <xpraxrig@gmail.com>
2023-07-05 21:06:04 -0400

    Test on Git add commit push

---
+++ src/gitAddCommitPush/TestGitandGitHub.java
@@ -0,0 +1,12 @@
+package gitAddCommitPush;
+
+public class TestGitandGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git hub test done!!");
+
+	}
+
+}
